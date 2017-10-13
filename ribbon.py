2dc04d94bf9a06136d5435b91cdc561e28f26365
danish <danishpa@gmail.com>
2017-10-13 20:46:02 +0000

    Ribbon working

--- ribbon.py
+++ ribbon.py
@@ -4,17 +4,23 @@ import time
 from display import Display
 
 LCD_DISPLAY_SIZE = 16
-INTERVAL = 0.3
+INTERVAL = 0.15
+SEPERATOR = '   '
 
-def ribbon(display, message, line=0, size=LCD_DISPLAY_SIZE):
+def ribbon(display, message, line=0, size=LCD_DISPLAY_SIZE, separator=SEPERATOR):
     if len(message) < size:
         display.write_line(message, line)
         return
 
-    buf = message + '  ' + message
-    for i in range(len(message) + 2):
-        time.sleep(INTERVAL)
-        display.write_line(buf[i:i+size], line)
+    buf = message + separator + message
+    while True:
+        for i in range(len(message) + len(separator)):
+            time.sleep(INTERVAL)
+            display.write_line(buf[i:i+size], line)
 
 if __name__ == '__main__':
-    ribbon(Display(), "this is my very very long message")
+    d = Display()
+    try:
+        ribbon(d, "this is my very long test message")
+    except KeyboardInterrupt:
+        d.clear()
